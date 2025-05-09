f080a3f2373324db0ceff37711d82174b7ab202b
Deepakshukla12 <your-email@example.com>
2025-05-09 19:41:53 +0530

    added code for events in react

--- demo_vite/src/ProductTab.jsx
+++ demo_vite/src/ProductTab.jsx
@@ -1,19 +1,25 @@
 // this is to unstand concept of props in react
 
-import Product from './Product.jsx';
+import Product from "./Product.jsx";
 
 function ProductTab() {
+  const action = () => {
+    alert("clicked");
+    console.log("clicked");
+  };
 
   // we can also assign a array of objects to a variable
   const features = ["hi-tech", "latest", "fast"];
   return (
-    <>
-    <Product title="laptop" features={features} />
-    <br />
-    <Product title="phone" price="40k" features={features}/>
-    <br />
-    <Product title="tablet" price="32k" features={features}/>
-    </>
+    <div>
+      <Product title="laptop" features={features} />
+      <br />
+      <Product title="phone" price="40k" features={features} />
+      <br />
+      <Product title="tablet" price="32k" features={features} />
+      <br />
+      <button onMouseOver={action}> Submit here</button>
+    </div>
   );
 }
 
