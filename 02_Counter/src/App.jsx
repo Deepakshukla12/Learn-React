74f5d5da1e729ecbef4d59efbe9dc3ce83f0c8f6
Deepakshukla12 <your-email@example.com>
2025-05-14 19:19:00 +0530

    added text state in this code

--- 02_Counter/src/App.jsx
+++ 02_Counter/src/App.jsx
@@ -1,44 +1,44 @@
-import './App.css'
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // using useState hook to create a state variable
+  let [counter, setCounter] = useState(15);
 
-  // using useState hook to create a state variable 
-  let [ counter, setCounter ]=useState(15);
-  
+  const [text, setText] = useState("Hello this is default text");
+
   //let counter = 15;
 
   const addValue = () => {
-    counter ++;
-    if( counter > 50 ){
+    counter++;
+    if (counter > 50) {
       alert("Counter value is greater than 50");
       return;
     }
     setCounter(counter);
-  }
+  };
 
   const removeValue = () => {
     counter--;
-    if(counter<1){
+    if (counter < 1) {
       alert("Counter value is less than 1");
       return;
     }
     setCounter(counter);
-  }
+  };
   return (
     <>
       <h1>Let's start the counter :</h1>
-      <h2>Counter value: {counter}</h2> 
+      <h2>Counter value: {counter}</h2>
 
-      <button
-        onClick={addValue} 
-      >Add Value</button>
-      <br /><br />
-      <button
-        onClick={removeValue}
-      >Subtract Value</button>
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <br />
+      <button onClick={removeValue}>Subtract Value</button>
+
+      <button onClick={() => setText("This is after clicking")}>{text}</button>
     </>
-  )
+  );
 }
 
 export default App;
