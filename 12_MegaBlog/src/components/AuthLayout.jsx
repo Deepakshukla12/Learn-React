09571f481be2d351e5d8e76a8264ce9c8b8217a0
Deepakshukla12 <your-email@example.com>
2025-05-30 18:39:22 +0530

    Updated code

--- 12_MegaBlog/src/components/AuthLayout.jsx
+++ 12_MegaBlog/src/components/AuthLayout.jsx
@@ -1,35 +1,3 @@
-// import React, {useState, useEffect} from 'react'
-// import {useSelector} from 'react-redux'
-// import {useNavigate} from 'react-router-dom'
-
-// export default function Protected({children, authentication = true}) {
-//     const navigate = useNavigate();
-//     const [loader, setLoader] = useState(true);
-
-//     const authStatus = useSelector(state => state.auth.status);
-
-//     useEffect(() => {
-//         // TODO : we can also make it easy and straight forward
-//         // if(authStatus===true){
-//         //     navigate('/')
-//         // }else if(authStatus===false){
-//         //     navigate('/login')
-//         // }
-
-//         if(authentication && authStatus !== authentication){
-//             navigate('/login');
-//         } else if(!authentication && authStatus !== authentication){
-//             navigate('/');
-//         }
-//         setLoader(false);
-
-//     }, [authStatus, navigate, authentication])
-//   return (
-//     loader ? <h1>Loading ...</h1> : <> {children} </>
-//   )
-// }
-
-
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
